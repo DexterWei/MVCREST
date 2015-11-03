9162b9165c9656949bbf428346733f3237b3dbea
dexterwei <dexterwei@163.com>
2015-11-03 15:01:50 -0800

    initial

---
+++ src/main/java/edu/sjsu/cmpe275/lab2/Application.java
@@ -0,0 +1,17 @@
+package edu.sjsu.cmpe275.lab2;
+
+/**
+ * Created by dexterwei on 11/3/15.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/edu/sjsu/cmpe275/lab2/GreetingController.java
@@ -0,0 +1,20 @@
+package edu.sjsu.cmpe275.lab2;
+
+/**
+ * Created by dexterwei on 11/3/15.
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
